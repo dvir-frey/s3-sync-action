b49aa02baf45b402d9c4569998308567a440e9c3
dvir-frey <58138117+dvir-frey@users.noreply.github.com>
2020-07-30 12:07:09 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,6 @@ ${AWS_ACCESS_INFO}
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
 sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
-              --profile s3-sync-action \
               --no-progress \
               ${ENDPOINT_APPEND} $*"
 
